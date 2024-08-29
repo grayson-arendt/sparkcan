5d1d2a939c2f4311b81dd97e606144f73fa51960
Grayson Arendt <grayson.n.arendt5@gmail.com>
2024-08-29 16:50:28 -0500

    Added source files for new classes

---
+++ src/SparkFlex.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file SparkFlex.cpp
+ * @brief Source file for the SparkFlex class for controlling REV Robotics SPARK Flex controllers
+ * @author Grayson Arendt
+ */
+
+
+#include "SparkFlex.hpp"
+
+SparkFlex::SparkFlex(const std::string &interfaceName, uint8_t deviceId)
+    : SparkBase(interfaceName, deviceId) {}
+
+SparkFlex::~SparkFlex() {}
---
+++ src/SparkMax.cpp
@@ -0,0 +1,6 @@
+#include "SparkMax.hpp"
+
+SparkMax::SparkMax(const std::string &interfaceName, uint8_t deviceId)
+    : SparkBase(interfaceName, deviceId) {}
+
+SparkMax::~SparkMax() {}
